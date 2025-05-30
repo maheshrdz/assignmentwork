1ba50af9cfa3b6a67aff22ea5357b9451316a18a
Vikas DZ <vikas.dz@smartData-612.local>
2025-05-30 15:40:09 +0530

    working on 0453 page

--- src/App.js
+++ src/App.js
@@ -5,7 +5,8 @@ import Home from './pages/home/Home';
 import Layout from './component/layout/Layout';
 import Ordermanagement from './pages/Ordermanagement';
 import Gastruckmanagement from './pages/Gastruckmanagement';
-import Gastruckmanagement from './pages/Gastruckmanagement';
+import SystemProgram from './pages/systemprogram/SystemProgram';
+
 
 function App() {
   return (
@@ -16,10 +17,7 @@ function App() {
           <Route path="/" element={<Layout />}>
             <Route index element={<Home />} />
             <Route path="/order-management" element={<Ordermanagement />} />
-            <Route
-              path="/gas-truck-management"
-              element={<Gastruckmanagement />}
-            />
+          
             <Route path="/system-program" element={<SystemProgram/>} />
           </Route>
         </Routes>
